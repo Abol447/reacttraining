72ee5afbb1c1051b7428ed32b0bd0783ab16f2cc
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-02-21 12:33:44 +0330

    popup finish

--- popup/src/App.jsx
+++ popup/src/App.jsx
@@ -20,74 +20,78 @@ const info = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciuntdolore aut. Eaque libero nisi ex asperiores doloribus recusandae fugain ipsa sit esse odio cum aut impedit ullam harum deleniti.",
   },
 ];
+const toogle = (flag) => (flag ? false : true);
 function App() {
   const [data, setData] = useState(info);
   const btnhandeller = (event) => {
     setData((x) =>
-      x.map((y) => (y.titr === event.target.id ? { ...y, open: !y.open } : y))
+      x.map((y) =>
+        y.titr === event.target.closest(".titr").id
+          ? { ...y, open: toogle(y.open) }
+          : { ...y, open: false }
+      )
     );
   };
-  {
-    console.log(data);
-  }
+
+  console.log(data);
   return (
     <>
       {data.map((x) => (
         <PopUp key={x.titr}>
-          <Titr>
+          <Titr id={x.titr}>
             <h1>{x.titr}</h1>
-            <svg
-              style={{ cursor: "pointer" }}
-              onClick={(event) => btnhandeller(event)}
-              id={x.titr}
-              width="20px"
-              height="20px"
-              viewBox="0 0 32 32"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"
-              fill="#000000"
-            >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <title>plus</title> <desc>Created with Sketch Beta.</desc>{" "}
-                <defs> </defs>{" "}
+            <div onClick={(event) => btnhandeller(event)}>
+              <svg
+                style={{ cursor: "pointer" }}
+                width="20px"
+                height="20px"
+                viewBox="0 0 32 32"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"
+                fill="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g
-                  id="Page-1"
-                  stroke="none"
-                  stroke-width="1"
-                  fill="none"
-                  fill-rule="evenodd"
-                  sketch:type="MSPage"
-                >
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
                   {" "}
+                  <title>plus</title> <desc>Created with Sketch Beta.</desc>{" "}
+                  <defs> </defs>{" "}
                   <g
-                    id="Icon-Set-Filled"
-                    sketch:type="MSLayerGroup"
-                    transform="translate(-362.000000, -1037.000000)"
-                    fill="#000000"
+                    id="Page-1"
+                    stroke="none"
+                    stroke-width="1"
+                    fill="none"
+                    fill-rule="evenodd"
+                    sketch:type="MSPage"
                   >
                     {" "}
-                    <path
-                      d="M390,1049 L382,1049 L382,1041 C382,1038.79 380.209,1037 378,1037 C375.791,1037 374,1038.79 374,1041 L374,1049 L366,1049 C363.791,1049 362,1050.79 362,1053 C362,1055.21 363.791,1057 366,1057 L374,1057 L374,1065 C374,1067.21 375.791,1069 378,1069 C380.209,1069 382,1067.21 382,1065 L382,1057 L390,1057 C392.209,1057 394,1055.21 394,1053 C394,1050.79 392.209,1049 390,1049"
-                      id="plus"
-                      sketch:type="MSShapeGroup"
+                    <g
+                      id="Icon-Set-Filled"
+                      sketch:type="MSLayerGroup"
+                      transform="translate(-362.000000, -1037.000000)"
+                      fill="#000000"
                     >
                       {" "}
-                    </path>{" "}
+                      <path
+                        d="M390,1049 L382,1049 L382,1041 C382,1038.79 380.209,1037 378,1037 C375.791,1037 374,1038.79 374,1041 L374,1049 L366,1049 C363.791,1049 362,1050.79 362,1053 C362,1055.21 363.791,1057 366,1057 L374,1057 L374,1065 C374,1067.21 375.791,1069 378,1069 C380.209,1069 382,1067.21 382,1065 L382,1057 L390,1057 C392.209,1057 394,1055.21 394,1053 C394,1050.79 392.209,1049 390,1049"
+                        id="plus"
+                        sketch:type="MSShapeGroup"
+                      >
+                        {" "}
+                      </path>{" "}
+                    </g>{" "}
                   </g>{" "}
-                </g>{" "}
-              </g>
-            </svg>
+                </g>
+              </svg>
+            </div>
           </Titr>
-          <div>{x.description}</div>
+          <div className={x.open ? "" : "hid"}>{x.description}</div>
         </PopUp>
       ))}
     </>
@@ -98,6 +102,10 @@ export default App;
 const PopUp = ({ children }) => {
   return <div className="form"> {children}</div>;
 };
-const Titr = ({ children }) => {
-  return <div className="titr">{children}</div>;
+const Titr = ({ id, children }) => {
+  return (
+    <div id={id} className="titr">
+      {children}
+    </div>
+  );
 };
